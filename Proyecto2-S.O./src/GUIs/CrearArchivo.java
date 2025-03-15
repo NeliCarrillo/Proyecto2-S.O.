a4aa766dc89d515eb5505f1f4aa94a609cf40808
SebasMarquez <sebastian.marquez@correo.unimet.edu.ve>
2025-03-14 20:42:35 -0400

    detalle

--- Proyecto2-S.O./src/GUIs/CrearArchivo.java
+++ Proyecto2-S.O./src/GUIs/CrearArchivo.java
@@ -34,7 +34,7 @@ public class CrearArchivo extends javax.swing.JFrame {
     
     private void actualizarDir(){
         Lista direc = this.sim.getDirectorios();
-        Nodo cabeza = direc.getPrimero();
+        Nodo cabeza = direc.getUltimo();
         while(cabeza!=null){
             this.directorio.addItem((String)cabeza.getDato());
             cabeza=cabeza.getSiguiente();
